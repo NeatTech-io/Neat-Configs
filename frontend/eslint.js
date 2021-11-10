33fb4d11524c3ad21e3e60fbf8bf781ed8b50a28
Vitaly <teivienn@gmail.com>
2021-11-10 18:27:58 +0300

    upd

--- frontend/eslint.js
+++ frontend/eslint.js
@@ -1,23 +1,44 @@
+const plugins = require('../core/plugins');
+const overrides = require('../core/overrides');
+const extendsOptions = require('../core/extends');
+const commonRules = require('../core/rules');
+
 module.exports = {
   env: {
     browser: true,
-    es6: true,
-    node: true,
+    es2021: true,
   },
+  "extends": [
+    ...extendsOptions['fontend/native'],
+    ...extendsOptions['common'],
+  ],
   parser: '@typescript-eslint/parser',
   parserOptions: {
-    ecmaVersion: 2018,
+    ecmaVersion: 13,
     sourceType: 'module',
     ecmaFeatures: {
       jsx: true,
     },
   },
-  overrides: [
-    {
-      files: ["*.js", "*.jsx"],
-      'rules': {
-        '@typescript-eslint/*': ['off']
-      }
-    }
+  "plugins": [
+    ...plugins['common'],
+    ...plugins['fontend/native'],
   ],
+  settings: {
+    'react': {
+      version: 'detect',
+    },
+    'import/resolver': {
+      node: {
+        paths: ['src'],
+      },
+    },
+  },
+  overrides: [
+    ...overrides,
+  ],
+  rules: {
+    ...commonRules['fontend/native'],
+    ...commonRules['common'],
+  }
 }
